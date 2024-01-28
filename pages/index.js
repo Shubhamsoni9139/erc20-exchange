0463527a4de8022edfc8a2bec6ebd30fc2fa5301
Shubham soni <ss4185667@gmail.com>
2024-01-29 00:36:55 +0530

    ui

--- pages/index.js
+++ pages/index.js
@@ -15,17 +15,19 @@ export default function Home() {
   }, [address]);
 
   return (
-    <div>
+    <>
+    <div className="body__container">
       <Head>
-        <title>Exchange TEST tokens</title>
+        <title >Exchange TEST tokens</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="home__container">
-        <h1>Sign in to exchange</h1>
+        <h1 >Sign in to exchange</h1>
         <button className="home__button" onClick={connectWithMetamask}>
           Sign in using MetaMask
         </button>
       </div>
     </div>
+    </>
   );
 }
